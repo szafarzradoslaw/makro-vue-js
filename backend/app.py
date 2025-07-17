f58b7d56c3c3c7ade6e88687a71a5d5a9d2fd685
szafarzradosław <radek10203@gmail.com>
2025-07-17 18:58:16 +0200

    now can get food info from date.josn

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-import json               # To read JSON files
+import json
 from flask import Flask, jsonify, request
 from flask_cors import CORS  # type: ignore # To allow cross-origin requests (frontend <-> backend)
 
@@ -6,19 +6,25 @@ app = Flask(__name__)
 CORS(app)  # Enable CORS so your frontend (usually on a different port) can request data
 
 # Load food data once on server startup
-with open('../database/food-macros.json', encoding='utf-8') as file:
+with open('../databases/food-macros.json', encoding='utf-8') as file:
     food_macros = json.load(file)  # This loads the JSON array as a Python list of dicts
 
-@app.route('/api/get-food-data/<name>', methods=['GET'])
-def get_food(name):
+@app.route('/api/get-food-data/<food>', methods=['GET'])
+def get_food(food):
     food_item = None
     for item in food_macros:
-        if item["product"].lower() == name.lower():
+        if item["product"].lower() == food.lower():
             food_item = item
     if food_item:
         return jsonify(food_item)
     else:
         return jsonify({"error": "Food not found"}), 404
+
+@app.route('/api/day-info/<day>', methods=['GET'])
+def day_info(day):
+    with open(f"../databases/{day}.json", encoding='utf-8') as file:
+        data = json.load(file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)  # Run the Flask server with debug mode on for easy testing
--- frontend/src/components/script.js
+++ frontend/src/components/script.js
@@ -1,8 +1,26 @@
 import { ref } from 'vue'
+
 const kalorieTotal = ref(0)
 const proteinTotal = ref(0)
 const fatTotal = ref(0)
 const carbsTotal = ref(0)
+
+async function getDayInfo() {
+  let Today = new Date().toISOString().slice(0, 10);
+  try{
+    const response = await fetch(`http://127.0.0.1:5000/api/day-info/${Today}`)
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+let test = await getDayInfo();
+console.log(test);
 
 async function getFood(food) {
   try {
@@ -19,10 +37,10 @@ async function getFood(food) {
 
 async function calculateMacros(food, quantity) {
   const data = await getFood(food);
-  kalorieTotal.value += (data.calories * quantity * 0.01)
-  proteinTotal.value += (data.protein * quantity * 0.01)
-  fatTotal.value += (data.fat * quantity * 0.01)
-  carbsTotal.value += (data.carbs * quantity * 0.01)
+  kalorieTotal.value += (data.calories * quantity * 0.01);
+  proteinTotal.value += (data.protein * quantity * 0.01);
+  fatTotal.value += (data.fat * quantity * 0.01);
+  carbsTotal.value += (data.carbs * quantity * 0.01);
 }
 
 export {
